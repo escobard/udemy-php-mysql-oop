f545c8368616ef6278bac09f7957d16b7dc00458
Daniel Escobar <escobard90@gmail.com>
2016-10-04 18:03:04 -0600

    PHP tags explained, and packages installed on sublime.

--- introduction/intro.php
+++ introduction/intro.php
@@ -1,1 +1,19 @@
-<?php ?>
+// this opening php statement will always work with PHP servers
+
+<?php 
+
+// each PHP statement must end with a semicolon. 
+// simple php string
+echo "Hello Student";
+
+
+
+?>
+
+// Other PHP closing statements some of these statements will not work with some apache versions. 
+
+<? ?>
+
+<?= ?>
+
+<% %>
