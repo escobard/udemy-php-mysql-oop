6935238d877c30862a418e2148fb4c73d2cf6c53
Daniel Escobar <escobard90@gmail.com>
2016-10-18 21:06:04 -0600

    End of math in PHP in the section 2 datatypes unit

--- section-2/math.php
+++ section-2/math.php
@@ -32,18 +32,22 @@ echo 45 + 34 * 45 / 425 - 45; // will do a calculation - without too much order 
 
 echo "<br>"; // space between equations
 
-echo 5 + 5 * 10; // order of operation - makes this operation wonky - as it stands it multiplies 10 x 5 then adds 5 equaling 55
+echo 5 + 5 * 10; // order of operation - makes this operation wonky - as it stands it multiplies 10 x 5 then adds 5 equaling 55. multiplication goes BEFORE addition
+
+echo 5 + 5 / 10 * 5; // order of operation - division also goes before addition , and before multiplication - this equals 15 in php and 20 on a normal calculator. always wrap math operations in parenthesis, to do proper math. VERY important
 
 echo "<br>"; // space between equations
 
 echo (5 + 5) * 10; // order of operation - proper order of operation, using basic algebra . does calculations in brackets first, then the operation on the right
 
+echo "<br>"; // space between equations
 
+// variable calculation
 
+$number1 = 12;
+$number2 = 24;
 
-
-
-
+echo $number1 + $number2; // adds the two variables
 
 
 
