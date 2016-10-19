79ac279d10b34e9c4dac058f5b8518b3ee6153f8
Daniel Escobar <escobard90@gmail.com>
2016-10-18 20:44:30 -0600

    First commit on PHP math operations

---
+++ section-2/math.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Math PHP exploration</title>
+</head>
+<body>
+
+<?php 
+
+echo 56 + 45; // can do calculations with spaces between each variable
+
+echo 56+45; // with no spaces
+
+echo 56+ 45; // with single space between variables
+
+echo "<br>"; // space between equations
+
+echo 56 - 45; // substractions
+
+echo "<br>"; // space between equations
+
+echo 56 * 45; // multiplications
+
+echo "<br>"; // space between equations
+
+echo 56 / 45; // division - data becomes a floating number
+
+echo "<br>"; // space between equations
+
+echo 45 + 34 * 45 / 425 - 45; // will do a calculation - without too much order of operation structure. 
+
+echo "<br>"; // space between equations
+
+echo 5 + 5 * 10; // order of operation - makes this operation wonky - as it stands it multiplies 10 x 5 then adds 5 equaling 55
+
+echo "<br>"; // space between equations
+
+echo (5 + 5) * 10; // order of operation - proper order of operation, using basic algebra . does calculations in brackets first, then the operation on the right
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
