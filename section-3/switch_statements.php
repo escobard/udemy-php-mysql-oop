cfb61b2e78597b20c845611b09d3f97afb928e03
Daniel Escobar <escobard90@gmail.com>
2016-10-28 16:02:38 -0600

    Section 3 switch statements lecture first commit

---
+++ section-3/switch_statements.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Switch Statements</title>
+</head>
+<body>
+	
+<?php 
+
+//basic switch statement
+//Useful for testing one condition against multiple values
+$number = 4;
+
+if($number <10){
+	echo "this";
+}
+
+?>
+
+</body>
+</html>
