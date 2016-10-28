deba1c2fa3d74ee8cc54bb33a52c330b01def879
Daniel Escobar <escobard90@gmail.com>
2016-10-27 22:12:39 -0600

    End of if / elseif / if statement lecture of section 3

--- section-3/if-statements.php
+++ section-3/if-statements.php
@@ -8,7 +8,7 @@
 
 <?php 
 
-// standard if statement in PHP
+/*// standard if statement in PHP
 if
 // arguments go in here. since 3 is less than 10, the if function will trigger
 (3 < 10)
@@ -31,17 +31,22 @@ else {
 
 echo "three is not less than ten";
 
-}
+} */
 
 // standard if / elseif /else statement
 // in an if / elseif statement, if statement argument is tested first
 if (3 < 10)
 {echo "three is less than ten";}
-else {echo "three is not less than ten";}
-
 //elseif is a single word unlike javascript's else if
-//elseif argument is tested second
-elseif (4 < 5) {}
+//elseif argument is tested after if statement. elseif statement ONLY runs if if statement is false
+elseif (4 > 5) {
+
+	echo "of course four is less than five";
+
+}
+
+// if both the if statement and the elseif statements are false, the else statement will execute
+else {echo "three is not less than ten";}
 ?>
 
 
