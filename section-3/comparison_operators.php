3139cfe51fdf019b8901034f9ad428f6ddaf182d
Daniel Escobar <escobard90@gmail.com>
2016-10-27 22:18:28 -0600

    first comparison operator commit of the section 3 comparison operators course

---
+++ section-3/comparison_operators.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Comparison Operators</title>
+</head>
+<body>
+	
+<?php 
+
+// standard comparison operator
+
+?>
+
+  <h2>Comparison Operators</h2>
+  <pre>
+      
+      equal ==
+      identical ===
+      compare > <  >= <= <>
+      not equal !=
+      not identical !==
+      
+</pre>
+
+</body>
+</html>
