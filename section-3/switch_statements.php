5eb7686ff3c620f66a9aa3b892586b129c62cb82
Daniel Escobar <escobard90@gmail.com>
2016-10-30 20:30:37 -0600

    First commit of the switch statement lecture of section 3

--- section-3/switch_statements.php
+++ section-3/switch_statements.php
@@ -7,15 +7,34 @@
 <body>
 	
 <?php 
-
+/*
 //basic switch statement
 //Useful for testing one condition against multiple values
+// if we wanted to test multiple arguments for a single variable
 $number = 4;
 
 if($number <10){
 	echo "this";
 }
 
+elseif($number => 10){
+	echo "this";
+}
+
+elseif($number > 5){
+	echo "this";
+}
+// but this doesn't work well as it creates too many if statements, taking up too much memory. for multiple conditional statements on the same variable, write a switch statement */
+
+switch (variable) {
+	case 'value':
+		# code...
+		break;
+	
+	default:
+		# code...
+		break;
+}
 ?>
 
 </body>
