f1a6d161046f971477a74fb394ffaad06dfa92b2
Daniel Escobar <escobard90@gmail.com>
2016-10-31 15:16:31 -0600

    Section 3 for loops lecture intro

---
+++ section-3/for_loops.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>For loops</title>
+</head>
+<body>
+
+<?php 
+
+// for loops
+// different than while loops because the incremental variable is created inside of the loop, and the increment function is added to the loop argument
+for ($counter = 0; $counter < 10; $counter++) { 
+	echo "<h2>This is a for loop</h2>" . "<br>";
+}
+
+?>
+
+</body>
+</html>
