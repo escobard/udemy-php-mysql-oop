3898c9a2f56a8486692327b533650dc5b0733e5c
Daniel Escobar <escobard90@gmail.com>
2016-10-31 15:31:43 -0600

    section 3 foreach loop start

---
+++ section-3/foreach_loop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Foreach loops</title>
+</head>
+<body>
+
+<?php 
+
+// for each loop is built to search arrays in PHP
+// Only works with arrays
+
+$numbers =  array(223,112,3442,231235,3246,922);
+
+// for each loops explained
+foreach (
+	// the first value would be the array
+	$numbers
+	// assigns the $number variable to each array value
+	as $number => $value) {
+	
+}
+
+?>
+
+</body>
+</html>
