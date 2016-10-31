2e0f0a514da81008f1372cb1240c2850aa916b01
Daniel Escobar <escobard90@gmail.com>
2016-10-31 15:03:58 -0600

    section 3 while loops lecture completed

--- section-3/while_loops.php
+++ section-3/while_loops.php
@@ -8,14 +8,34 @@
 	
 <?php 
 
-// while loops. 
+// create a counter variable that increments every time the while loop runs
+
+
+/*// while loops. 
 while (4 < 10) {
 
 // this means that as long as 4 is less than 10, the while loop statement will execute.
 // In other words, we just created an infinite loop, that continues running forever. 
 	echo "Hello Teachers";
-// could break after first stament, which i one way to end a while loop
- break;
+
+// could break after first stament, which i one way to end a while loop.
+// break;
+} */
+
+// create a counter variable that increments every time the while loop runs
+$counter = 0;
+
+// this means that as long as counter is less than 10, the while loop statement will execute.
+// while loops are used to do things multiple times depending on the argument
+while ($counter <= 10) {
+	echo $counter . "<br>";
+
+// To stop the infinite loop, we have incremented the counter variable by 1 every time the loop is executed.
+// with this method, this while loop runs 10 times before it stops.
+// $counter = $counter + 1; is another way of incrementing the variable, but the increment function is smarter
+
+// used an increment operator instead, which is the built in function for incrementation
+    $counter++;
 }
 
 ?>
