e04158b1dd58933cbcc056c5bf7bf1da609a6b10
Daniel Escobar <escobard90@gmail.com>
2016-10-31 14:51:15 -0600

    Broke infinite loop with a break function.

--- section-3/while_loops.php
+++ section-3/while_loops.php
@@ -14,6 +14,8 @@ while (4 < 10) {
 // this means that as long as 4 is less than 10, the while loop statement will execute.
 // In other words, we just created an infinite loop, that continues running forever. 
 	echo "Hello Teachers";
+// could break after first stament, which i one way to end a while loop
+ break;
 }
 
 ?>
