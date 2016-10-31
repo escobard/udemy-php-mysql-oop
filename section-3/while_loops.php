a991ef912f35d913b18ba0b397203c74aca703b9
Daniel Escobar <escobard90@gmail.com>
2016-10-31 14:49:27 -0600

    Section 3 while loop infinite loop created

---
+++ section-3/while_loops.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>While Loops</title>
+</head>
+<body>
+	
+<?php 
+
+// while loops. 
+while (4 < 10) {
+
+// this means that as long as 4 is less than 10, the while loop statement will execute.
+// In other words, we just created an infinite loop, that continues running forever. 
+	echo "Hello Teachers";
+}
+
+?>
+</body>
+</html>
