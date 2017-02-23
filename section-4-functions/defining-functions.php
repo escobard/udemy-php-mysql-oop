14c36a8733d40ef80304cb5716602f0d1f050d30
Daniel Escobar <escobard90@gmail.com>
2017-02-22 22:31:14 -0700

    type: feat
    section 4 lesson 1 defining functions completed

---
+++ section-4-functions/defining-functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<?php 
+
+// defining a function
+
+function say_Something (){
+
+echo 'Hello, testing a function';
+
+}
+
+// funciton must be called to render in the DOM, exactly like JS
+
+say_Something();
+
+?>
+
+</body>
+</html>
