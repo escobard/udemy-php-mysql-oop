6d8f21d1a28ff345890c8a25cb0eddffbf785fe7
Daniel Escobar <escobard90@gmail.com>
2017-02-23 01:21:38 -0700

    type: feat
    section 4 lesson 2 function parameters complete

--- section-4-functions/defining-functions.php
+++ section-4-functions/defining-functions.php
@@ -8,16 +8,28 @@
 
 <?php 
 
-// defining a function
+// declaring a function
 
 function say_Something (){
 
-echo 'Hello, testing a function';
+	echo 'Hello, testing a function, other functions echoed below' . '<br>';
+	echo 'this is the calculate function' . calculate() . '<br>';
+	echo "this is the text function" . text() . '<br>';
+
+}
+
+function calculate(){
+
+	echo 4455+1000;
+}
+
+function text(){
+
+	echo 'testing text string';
 
 }
 
 // funciton must be called to render in the DOM, exactly like JS
-
 say_Something();
 
 ?>
