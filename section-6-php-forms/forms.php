0cf418e765b2bc20154ba5a281b872654ad0d980
Daniel Escobar <escobard90@gmail.com>
2017-02-25 17:31:32 -0700

    type: feat
    section 6 lesson 2 extracting form data from PHP forms

--- section-6-php-forms/forms.php
+++ section-6-php-forms/forms.php
@@ -4,8 +4,16 @@
 // isset checks if the variable is null, more on that here : http://php.net/manual/en/function.isset.php
 // the action of the form, which is forms.php, is submited via a POST method. The method forms.php, is this document, which
 // is reloaded loaded when the form submits
+// the form method 'post' pushes the values of the form into the $_POST super global, where we can grab them
 if (isset($_POST['submit'])) {
-	echo "form works";
+	echo "form works!".'<br>';
+	// grabs the values of the name, places it in a variable.
+	$name = $_POST['name'];
+	$password = $_POST['password'];
+
+	echo 'This is the name: '.$name.'<br>';
+	echo 'This is the password: '.$password.'<br>';
+
 }
 
 ?>
